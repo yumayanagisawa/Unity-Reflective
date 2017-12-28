0e1faf0e6f1101cb6d61ed52b8b37533ff290172
Yuma Yanagisawa <ya7gisa0@gmail.com>
2017-12-29 07:01:16 +0900

    initial commit

---
+++ Reflective/Assets/Scripts/MaterialController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MaterialController : MonoBehaviour {
+    private Material mat;
+
+	// Use this for initialization
+	void Start () {
+        mat = GetComponent<Renderer>().material;//.SetFloat("_BumpScale", -0.5f);
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        mat.SetFloat("_BumpScale", Mathf.Sin(Time.time)*1.5f);
+    }
+}
---
+++ Reflective/Assets/Scripts/MeshNormalManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public class MeshNormalManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        Mesh mesh = GetComponent<MeshFilter>().mesh;
+        mesh.triangles = mesh.triangles.Reverse().ToArray();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
